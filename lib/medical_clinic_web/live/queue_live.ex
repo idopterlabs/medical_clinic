561972ce411c1bb4b23d6d34da8b88d44edbc71a
Carlos Souza <carloshrsouza@gmail.com>
2023-12-16 11:53:18 -0500

    Extract to LiveComponent.
    This enables proper HTML diffing.

--- lib/medical_clinic_web/live/queue_live.ex
+++ lib/medical_clinic_web/live/queue_live.ex
@@ -2,6 +2,7 @@ defmodule MedicalClinicWeb.QueueLive do
   use MedicalClinicWeb, :live_view
 
   alias MedicalClinic.Appointments
+  alias MedicalClinicWeb.AppointmentEntry
 
   @topic "check-in"
   @pub_sub MedicalClinic.PubSub
@@ -36,31 +37,8 @@ defmodule MedicalClinicWeb.QueueLive do
 
   def render(assigns) do
     ~H"""
-    <div
-      :for={appointment <- assigns.appointments}
-      class={[
-        "rounded w-4/5 mx-auto my-2 p-10 h-24 flex items-center place-content-between bg-blue-300",
-        if(appointment.is_checked_in, do: "bg-green-200")
-      ]}
-    >
-      <p class="text-teal-900 font-light text-3xl"><%= appointment.patient.name %></p>
-      <p class="text-blue-900 text-sm">
-        <div class="flex container text-xs flex-col text-right">
-          <div :if={appointment.is_checked_in} class="p-2 font-bold text-xl">
-            Checked-in!
-          </div>
-          <div :if={!appointment.is_checked_in} class="p-2">
-            <button
-              class="bg-blue-600 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition-colors duration-300"
-              phx-value-id={appointment.id}
-              data-confirm="Are you sure ?"
-              phx-click="check-in"
-            >
-              Check-in
-            </button>
-          </div>
-        </div>
-      </p>
+    <div :for={appointment <- assigns.appointments}>
+      <.live_component module={AppointmentEntry} id={appointment.id} appointment={appointment} />
     </div>
     """
   end
